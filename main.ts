5ead1870dbd8f200683fb97f937770182d1458f3
syedaureefaaly <161452433+syedaureefaaly@users.noreply.github.com>
2024-03-04 00:13:12 +0500

    Add files via upload
    My 5 ans

---
+++ main.ts
@@ -0,0 +1,25 @@
+//Question1.
+let myName:string = "hello ureefaaly, would you like to learn some typescript ?";
+console.log(myName);
+
+//Question2.
+let personsName: string = "ureefaaly";
+console.log(personsName. toUpperCase());
+console.log(personsName. toLowerCase());
+
+
+// Question3.
+console.log("Benjamin franklin once said, well done is better than well said");
+
+//Question4.
+let famousName: string ="Benjamin franklin";
+let message: "once said well done is better than well said";
+console.log( famousName, "once said well done is better than well said.");
+
+// Question5.
+let name1:  string =  "\t\n ureefa \t\n";
+console.log(name1);
+console.log(name1 .trim());
+
+
+
